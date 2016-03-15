3440b289d5d2855945091b736836f4e96af8cf03
Mike Gehard <mgehard@pivotal.io>
2016-03-15 11:52:53 -0600

    Fix mismatched parameter for telling broker to run async
    The CC sends the following call to the broker to signal async:
    /v2/service_instances/23c3d9c3-ff96-4bff-8418-d0503eb0f330?accepts_incomplete=true&plan_id=541ae588-0af6-4e63-ba54-4f7a8972e30d&service_id=e31aadf0-053f-4678-a8d0-b1e62ab78078
    
    We need to look for the accepts_incomplete parameter and not async.

--- src/test/java/org/springframework/cloud/servicebroker/controller/ServiceInstanceControllerIntegrationTest.java
+++ src/test/java/org/springframework/cloud/servicebroker/controller/ServiceInstanceControllerIntegrationTest.java
@@ -436,7 +436,7 @@ public class ServiceInstanceControllerIntegrationTest extends ControllerIntegrat
 		return uriBuilder.path(request.getServiceInstanceId())
 				.queryParam("service_id", request.getServiceDefinitionId())
 				.queryParam("plan_id", request.getPlanId())
-				.queryParam("async", request.isAsyncAccepted())
+				.queryParam("accepts_incomplete", request.isAsyncAccepted())
 				.toUriString();
 	}
 
