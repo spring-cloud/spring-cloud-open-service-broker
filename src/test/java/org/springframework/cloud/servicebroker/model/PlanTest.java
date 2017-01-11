d2246ed6439dbd2627ad44c93f4f2cedfbe2c28a
Sebastien Bortolussi <sebastien.bortolussi@orange.com>
2017-01-11 15:45:23 +0100

    Service Plan is free by default
    These changes make Service Plan free by default as documented at https://docs.cloudfoundry.org/services/api.html
    
    [#25]

---
+++ src/test/java/org/springframework/cloud/servicebroker/model/PlanTest.java
@@ -0,0 +1,14 @@
+package org.springframework.cloud.servicebroker.model;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class PlanTest {
+
+    @Test
+    public void servicePlanIsFreeByDefault() throws Exception {
+        final Plan plan = new Plan();
+        assertEquals(Boolean.TRUE, plan.isFree());
+    }
+}
