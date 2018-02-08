9702ed076e8de929b782a13790ce61bb4b730af0
Roy Clarkson <rclarkson@pivotal.io>
2018-02-07 19:43:42 -0500

    Add initial support for Spring Cloud Contract tests
    Closes #58

---
+++ spring-cloud-open-service-broker-autoconfigure/src/test/java/org/springframework/cloud/servicebroker/autoconfigure/contract/CatalogBase.java
@@ -0,0 +1,59 @@
+/*
+ * Copyright 2002-2018 the original author or authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.springframework.cloud.servicebroker.autoconfigure.contract;
+
+import io.restassured.module.mockmvc.RestAssuredMockMvc;
+import org.junit.Before;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import org.springframework.cloud.servicebroker.controller.CatalogController;
+import org.springframework.cloud.servicebroker.model.Catalog;
+import org.springframework.cloud.servicebroker.model.Plan;
+import org.springframework.cloud.servicebroker.model.ServiceDefinition;
+import org.springframework.cloud.servicebroker.service.CatalogService;
+
+import static org.mockito.Mockito.when;
+
+public class CatalogBase {
+
+	@Mock
+	private CatalogService catalogService;
+
+	@Before
+	public void setup() {
+		Catalog catalog = Catalog.builder()
+				.serviceDefinitions(
+						ServiceDefinition.builder()
+								.id("service-definition-id-one")
+								.name("service-definition-one")
+								.description("Service Definition One")
+								.plans(Plan.builder().build())
+								.build(),
+						ServiceDefinition.builder()
+								.id("service-definition-id-two")
+								.name("service-definition-two")
+								.description("Service Definition Two")
+								.plans(Plan.builder().build())
+								.build())
+				.build();
+
+		MockitoAnnotations.initMocks(this);
+		when(this.catalogService.getCatalog()).thenReturn(catalog);
+		RestAssuredMockMvc.standaloneSetup(new CatalogController(this.catalogService));
+	}
+
+}
