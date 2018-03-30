2c40dd0831d19fbc1ce38ca4c47604e4bfd06330
Roy Clarkson <rclarkson@pivotal.io>
2018-03-30 13:23:32 -0400

    Execute contract tests against WebFlux and Spring MVC

--- spring-cloud-open-service-broker-autoconfigure/src/test/resources/contracts/catalog/servlet/retrieveInstanceCatalog.groovy
+++ spring-cloud-open-service-broker-autoconfigure/src/test/resources/contracts/catalog/servlet/retrieveInstanceCatalog.groovy
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *       http://www.apache.org/licenses/LICENSE-2.0
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,7 +13,7 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package catalog
+package catalog.servlet
 
 org.springframework.cloud.contract.spec.Contract.make {
 	request {
@@ -25,7 +25,7 @@ org.springframework.cloud.contract.spec.Contract.make {
 	}
 	response {
 		status 200
-		body(file("fixture/retrieve_catalog_response.json"))
+		body(file("retrieve_catalog_response.json"))
 		headers {
 			contentType('application/json')
 		}
