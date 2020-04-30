b7b739da0a276213d4ce444cc026831abc173242
Roy Clarkson <rclarkson@vmware.com>
2020-04-30 12:21:21 -0400

    Update javadoc to reflect correct HTTP status response for thrown exceptions
    Closes #279

--- spring-cloud-open-service-broker-core/src/main/java/org/springframework/cloud/servicebroker/exception/ServiceInstanceBindingDoesNotExistException.java
+++ spring-cloud-open-service-broker-core/src/main/java/org/springframework/cloud/servicebroker/exception/ServiceInstanceBindingDoesNotExistException.java
@@ -20,8 +20,12 @@ package org.springframework.cloud.servicebroker.exception;
  * Thrown to indicate that a request includes a service binding ID that is not known to the service broker.
  *
  * <p>
- * Throwing this exception will result in an HTTP status code {@literal 422 UNPROCESSABLE ENTITY} being returned to the
- * platform.
+ * Throwing this exception will result in different HTTP status codes being returned to the platform, depending on
+ * the condition. The default HTTP status code is {@literal 422 UNPROCESSABLE ENTITY}. If this exception is thrown
+ * when retrieving a service instance binding, an HTTP status {@literal 404 NOT FOUND} is returned. If this exception
+ * is thrown when deleting a service instance binding, an HTTP status {@literal 410 GONE} is returned.
+ *
+ * @see org.springframework.cloud.servicebroker.controller.ServiceInstanceBindingController
  */
 public class ServiceInstanceBindingDoesNotExistException extends ServiceBrokerException {
 
--- spring-cloud-open-service-broker-core/src/main/java/org/springframework/cloud/servicebroker/exception/ServiceInstanceDoesNotExistException.java
+++ spring-cloud-open-service-broker-core/src/main/java/org/springframework/cloud/servicebroker/exception/ServiceInstanceDoesNotExistException.java
@@ -20,10 +20,15 @@ package org.springframework.cloud.servicebroker.exception;
  * Thrown to indicate that a request includes a service instance ID that is not known to the service broker.
  *
  * <p>
- * Throwing this exception will result in an HTTP status code {@literal 422 UNPROCESSABLE ENTITY} being returned to the
- * platform.
+ * Throwing this exception will result in different HTTP status codes being returned to the platform, depending on
+ * the condition. The default HTTP status code is {@literal 422 UNPROCESSABLE ENTITY}. If this exception is thrown
+ * when retrieving a service instance or service instance binding, an HTTP status {@literal 404 NOT FOUND} is
+ * returned. If this exception is thrown when deleting a service instance, an HTTP status {@literal 410 GONE} is
+ * returned.
  *
  * @author sgreenberg@pivotal.io
+ * @see org.springframework.cloud.servicebroker.controller.ServiceInstanceController
+ * @see org.springframework.cloud.servicebroker.controller.ServiceInstanceBindingController
  */
 public class ServiceInstanceDoesNotExistException extends ServiceBrokerException {
 
