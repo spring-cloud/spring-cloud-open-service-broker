01413733fe6bdf87129398944aae49f62dd163c1
Roy Clarkson <rclarkson@vmware.com>
2021-08-31 18:06:58 -0400

    Add initial concourse pipeline

---
+++ ci/scripts/build-project.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+set -euo pipefail
+
+readonly SKIP_TESTS="${SKIP_TESTS:-false}"
+
+# shellcheck source=common.sh
+source "$(dirname "$0")/common.sh"
+repository=$(pwd)/distribution-repository
+if [ "$SKIP_TESTS" == "true" ]; then
+	build_task=assemble
+else
+	build_task=build
+fi
+
+pushd git-repo >/dev/null
+./gradlew --parallel clean "$build_task" publish \
+	-PpublicationRepository="${repository}"
+popd >/dev/null
