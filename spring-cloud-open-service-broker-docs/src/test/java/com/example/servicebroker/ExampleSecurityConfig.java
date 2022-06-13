920c2fb5d3709fdc884cacb0d93d3ec505ee87e2
Roy Clarkson <rclarkson@vmware.com>
2022-06-13 18:42:50 -0400

    Upgrade to Spring Boot 2.7.0
    Closes #351

--- spring-cloud-open-service-broker-docs/src/test/java/com/example/servicebroker/ExampleSecurityConfig.java
+++ spring-cloud-open-service-broker-docs/src/test/java/com/example/servicebroker/ExampleSecurityConfig.java
@@ -4,23 +4,24 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 @EnableWebSecurity
-public class ExampleSecurityConfig extends WebSecurityConfigurerAdapter {
+public class ExampleSecurityConfig {
 
-	@Override
-	protected void configure(HttpSecurity http) throws Exception {
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 		http
 				.csrf().disable()
 				.authorizeRequests()
 				.antMatchers("/v2/**").hasRole("ADMIN")
 				.and()
 				.httpBasic();
+		return http.build();
 	}
 
 	@Bean
