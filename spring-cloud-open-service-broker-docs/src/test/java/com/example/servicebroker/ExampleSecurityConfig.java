a427076898db331020c23c8b7996e02869fe91e7
Gareth Clay <gclay@vmware.com>
2023-09-19 10:51:35 +0100

    Remove deprecated method calls from ExampleSecurityConfig

--- spring-cloud-open-service-broker-docs/src/test/java/com/example/servicebroker/ExampleSecurityConfig.java
+++ spring-cloud-open-service-broker-docs/src/test/java/com/example/servicebroker/ExampleSecurityConfig.java
@@ -2,8 +2,10 @@ package com.example.servicebroker;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
@@ -13,17 +15,12 @@ import org.springframework.security.web.SecurityFilterChain;
 @EnableWebSecurity
 public class ExampleSecurityConfig {
 
-	// TODO: Update the Spring Security Example
 	@Bean
-	@SuppressWarnings("removal")
 	public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-		http
-				.csrf().disable()
-				.authorizeHttpRequests()
-				.requestMatchers("/v2/**").hasRole("ADMIN")
-				.and()
-				.httpBasic();
-		return http.build();
+		return http.csrf(AbstractHttpConfigurer::disable)
+				.authorizeHttpRequests(httpRequests -> httpRequests.requestMatchers("/v2/**").hasRole("ADMIN"))
+				.httpBasic(Customizer.withDefaults())
+				.build();
 	}
 
 	@Bean
