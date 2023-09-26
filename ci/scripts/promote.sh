26f8eb44443e78d80b9392fa8f307e5857e41553
Gareth Clay <gclay@vmware.com>
2023-09-26 17:35:31 +0100

    Use concourse-release-scripts image instead of JAR
    The release version of concourse-release-scripts.jar is no longer
    publicly available, so switch to using the image instead.

--- ci/scripts/promote.sh
+++ ci/scripts/promote.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-# shellcheck source=scripts/common.sh
-source $(dirname $0)/common.sh
+readonly BUILD_INFO_LOCATION="$(pwd)/artifactory-repo/build-info.json"
+readonly VERSION=$( jq -r '.buildInfo.modules[0].id' < "$BUILD_INFO_LOCATION" | sed 's/.*:.*:\(.*\)/\1/' )
 
-version=$( cat artifactory-repo/build-info.json | jq -r '.buildInfo.modules[0].id' | sed 's/.*:.*:\(.*\)/\1/' )
-export BUILD_INFO_LOCATION=$(pwd)/artifactory-repo/build-info.json
-
-java -jar /opt/concourse-release-scripts*.jar promote $RELEASE_TYPE $BUILD_INFO_LOCATION
+java -jar /concourse-release-scripts.jar promote "$RELEASE_TYPE" "$BUILD_INFO_LOCATION"
 
 echo "Promotion complete"
-echo $version > version/version
+echo "$VERSION" > version/version
--- ci/scripts/sync-to-maven-central.sh
+++ ci/scripts/sync-to-maven-central.sh
@@ -4,8 +4,8 @@ set -euo pipefail
 readonly BUILD_INFO_LOCATION="$(pwd)/artifactory-repo/build-info.json"
 readonly CONFIG_DIR="$(pwd)/git-repo/ci/config"
 
-java -jar /opt/concourse-release-scripts*.jar \
-  --spring.config.location="${CONFIG_DIR}/release-scripts.yml" \
+java -jar /concourse-release-scripts.jar \
+  --spring.config.location="$CONFIG_DIR/release-scripts.yml" \
   publishToCentral 'RELEASE' "$BUILD_INFO_LOCATION" "artifactory-repo"
 
 echo "Sync complete"
