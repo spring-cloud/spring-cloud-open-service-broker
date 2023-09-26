405ac378f1712168e880c998bad8168570d30d58
Gareth Clay <gclay@vmware.com>
2023-09-26 18:18:16 +0100

    promote.sh - remove use of get_revision_from_buildinfo function

--- ci/scripts/promote.sh
+++ ci/scripts/promote.sh
@@ -1,13 +1,10 @@
 #!/bin/bash
 set -euo pipefail
 
-pushd artifactory-repo > /dev/null
-version=$( get_revision_from_buildinfo )
-popd > /dev/null
-
 readonly BUILD_INFO_LOCATION="$(pwd)/artifactory-repo/build-info.json"
+readonly VERSION=$( jq -r '.buildInfo.modules[0].id' < "$BUILD_INFO_LOCATION" | sed 's/.*:.*:\(.*\)/\1/' )
 
 java -jar /concourse-release-scripts.jar promote "$RELEASE_TYPE" "$BUILD_INFO_LOCATION"
 
 echo "Promotion complete"
-echo "$version" > version/version
+echo "$VERSION" > version/version
